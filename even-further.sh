ddfb33b01b9c045fbd3f574af74352c21a7a7281
Zak B. Elep <zakame@zakame.net>
2018-11-15 12:44:55 +0800

    More sudo

--- even-further.sh
+++ even-further.sh
@@ -6,3 +6,8 @@ echo "Whoami at $0"
 sudo whoami
 
 sudo apt-get install -y emacs
+
+sudo ifconfig lo:1 172.28.128.3
+sudo /bin/su -c "echo -n dis.somewhere.com > /etc/hostname"
+sudo /bin/su -c "echo '172.28.12.3 dis.somewhere.com' >> /etc/hosts"
+sudo hostname dis.somewhere.com
